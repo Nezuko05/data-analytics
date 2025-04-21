38c381c530d508b4e119ccae5f5b2467fdfa0a80
Riya Arora <riyaarora0517@gmail.com>
2025-04-21 14:31:57 +0530

    data

--- Dashboard/home.py
+++ Dashboard/home.py
@@ -1,1 +1,5 @@
 import streamlit as st
+st.markdown('<h1 style=" color:white ; background-color:green ; text-align:center;">''Titanic Data Analysis</h1>', unsafe_allow_html=True)
+st.write("This is a simple Streamlit app to analyze the Titanic dataset")
+
+st.image("Assets/car.png")
--- Dashboard/pages/dashboard.py
+++ Dashboard/pages/dashboard.py
@@ -6,12 +6,43 @@ import seaborn as sns
 
 st.title("Dashboard")
 df = sns.load_dataset("Titanic")
+#to display dataset
 st.dataframe(df)
 
-fig = px.histogram(df, x='age',color='sex', facet_col='survived',
+
+#gender filter
+gender_filter=st.sidebar.multiselect("Gender",options = df["sex"].unique(),default=df["sex"].unique())
+filtered_df=df[
+    (df["sex"].isin(gender_filter))
+]
+
+#pclass filter
+pclass_filter= st.sidebar.multiselect("Passenger Class",options=df["pclass"].unique(),default = df["pclass"].unique())
+
+
+
+fig = px.histogram(filtered_df, x='age',color='sex', facet_col='survived',
               title='age distribution by gender and survived',
               
               template='plotly_dark',width=800)
  
 
 st.plotly_chart(fig)
+st.markdown("This graph shows age distribution by gender and survived ")
+
+
+fig = px.scatter(filtered_df,x='age',y='fare', color='survived', title='Age vs Fare by Survival',
+           template='plotly_dark', width=809)
+
+st.plotly_chart(fig)
+st.markdown("This graph shows age vs fare by survival ")
+
+
+
+fig = px.sunburst(filtered_df, path=['pclass','sex','survived'],
+            title='Survived by class and gender', template='plotly_dark',
+            width=800)
+
+st.plotly_chart(fig)
+st.markdown("This graph shows survived by class and gender ")
+
